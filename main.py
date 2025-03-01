5fd9c34f5c0399c05947f83d6f8881ed3c38f8f7
Jukelyn <viperkillergaming@gmail.com>
2025-02-28 23:11:53 -0500

    Renamed output file

--- main.py
+++ main.py
@@ -17,6 +17,6 @@ with open("diy_codes_output.txt", "w", encoding="utf-8") as output_file:
     for code in diy_codes:
         output_file.write(code.lower() + "\n")
 
-with open('output.txt', 'w', encoding="utf-8") as f:
+with open('grouped_diy_codes_output.txt', 'w', encoding="utf-8") as f:
     for i in range(0, len(diy_codes), 40):
         f.write(" ".join(diy_codes[i:i+40]) + "\n")
