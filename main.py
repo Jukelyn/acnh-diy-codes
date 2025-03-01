440bf8dd7a3406ab718355d1f8e570e6c31f1906
Jukelyn <viperkillergaming@gmail.com>
2025-02-28 23:11:11 -0500

    Updated and cleaned up, documented somewhat

--- main.py
+++ main.py
@@ -1,30 +1,22 @@
-import csv
+import re
+
+with open("list.txt", "r", encoding="utf-8") as file:
+    content = file.readlines()
 
 
-hex_codes = []
+pattern = re.compile(r"DIY:\s([0-9A-F]{4})")
 
-with open('diys.csv', mode='r', encoding="utf-8") as file:
-    csv_reader = csv.reader(file)
-    next(csv_reader)  # Skip header row
-    for code in csv_reader:
-        hex_codes.append(code[1].lower())
+# Extract DIY codes
+diy_codes = [match.group(1)
+             for line in content for match in pattern.finditer(line)]
 
-with open('diy_codes_output.txt', 'r', encoding="utf-8") as diy_file:
-    diy_codes = diy_file.read().splitlines()
+for i, code in enumerate(diy_codes):
+    diy_codes[i] = code[-3:] + "000016A2"
 
-for code in hex_codes:
-    if code[-3:] in diy_codes:
-        # code[-3:]
-        pass
-    else:
-        print(f"{code[-3:]} not in other file")
-
-output = []
-# Get last 3 characters, put at front
-# and 16A2 at the end with 0000 in between
-for code in hex_codes:
-    output.append(code[-3:] + "000016A2")
+with open("diy_codes_output.txt", "w", encoding="utf-8") as output_file:
+    for code in diy_codes:
+        output_file.write(code.lower() + "\n")
 
 with open('output.txt', 'w', encoding="utf-8") as f:
-    for i in range(0, len(output), 40):
-        f.write(" ".join(output[i:i+40]) + "\n")
+    for i in range(0, len(diy_codes), 40):
+        f.write(" ".join(diy_codes[i:i+40]) + "\n")
