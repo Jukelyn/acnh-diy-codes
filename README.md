e048c64037a2775585d852a6d20c647ed0312134
Jukelyn <viperkillergaming@gmail.com>
2025-02-28 23:18:02 -0500

    Code comments and some documentation in README

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
-[Dataset Link](https://mpql.net/tools/acnh/codes/item-list/)
+# ACNH DIY Codes for OmegaToGo Bots
+This project takes all the possible DIY recipes as reported in the dataset [here](https://mpql.net/tools/acnh/codes/item-list/) and outputs the codes in the format that lets them be used in the OmegaToGo bots in the NooksTreasureIslands Discord.
 
+## Information
 `list.txt` is the list of all the items with the DIY IDs included.
 
 `diy_codes_output.txt` is 1 DIY codes per line
 
 `output.txt` is the DIY codes in groups of 40 per line
+
+
+## Usage
+`python main.py`: Runs the script.
--- main.py
+++ main.py
@@ -1,22 +1,30 @@
+"""This script takes all the possible DIY recipes as reported in the dataset
+[here](https://mpql.net/tools/acnh/codes/item-list/) and outputs the codes in
+the format that lets them be used in the OmegaToGo bots in the
+NooksTreasureIslands Discord.
+"""
 import re
 
 with open("list.txt", "r", encoding="utf-8") as file:
     content = file.readlines()
 
-
+# Pattern to match ("DIY: CODE")
 pattern = re.compile(r"DIY:\s([0-9A-F]{4})")
 
 # Extract DIY codes
 diy_codes = [match.group(1)
              for line in content for match in pattern.finditer(line)]
 
+# Change the array elements to use the proper format
 for i, code in enumerate(diy_codes):
     diy_codes[i] = code[-3:] + "000016A2"
 
+# Output codes 1 per line
 with open("diy_codes_output.txt", "w", encoding="utf-8") as output_file:
     for code in diy_codes:
         output_file.write(code.lower() + "\n")
 
+# Output codes 40 per line (for max inventory requests)
 with open('grouped_diy_codes_output.txt', 'w', encoding="utf-8") as f:
     for i in range(0, len(diy_codes), 40):
         f.write(" ".join(diy_codes[i:i+40]) + "\n")
