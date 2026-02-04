0c3fcf68a49666579cc6a0988a835809790d53c7
Jukelyn <a.mehraz.cs@gmail.com>
2026-02-04 06:11:28 -0500

    Update dockerfile and use standalone output

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,7 @@ const nextConfig: NextConfig = {
   reactCompiler: true,
   turbopack: {},
   allowedDevOrigins: ["http://192.168.8.106:3000"], // your IP + port
+  output: "standalone",
 };
 
 export default nextConfig;
